8cc9ea96a386c2786bb452716c6bba8ea7f8a637
Maksymilian Chwałek <mchwalek89@gmail.com>
2026-07-02 20:14:41 +0200

    feat(server): listen on all interfaces (0.0.0.0:5000)

--- src/Edzio.SignalingServer/Program.cs
+++ src/Edzio.SignalingServer/Program.cs
@@ -2,6 +2,7 @@ using Edzio.SignalingServer.Hubs;
 using Edzio.SignalingServer.Services;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.WebHost.UseUrls("http://0.0.0.0:5000");
 builder.Services.AddSignalR();
 builder.Services.AddSingleton<IPairingCodeService, PairingCodeService>();
 
